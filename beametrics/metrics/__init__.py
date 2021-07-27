b94ea0055aabf352942b8c421411d374ab669ff9
thomasScialom <thomas@recital.ai>
2021-07-27 17:07:12 +0200

    clean README.md

--- beametrics/metrics/__init__.py
+++ beametrics/metrics/__init__.py
@@ -1,5 +1,5 @@
 from beametrics.metrics.metrics import *
-from beametrics.metrics.metrics_questeval import *
+#from beametrics.metrics.metrics_questeval import *
 
 _D_METRICS = {
     MetricSacreBleu.metric_name(): MetricSacreBleu,
@@ -8,15 +8,5 @@ _D_METRICS = {
     MetricSari.metric_name(): MetricSari,
     MetricBertscore.metric_name(): MetricBertscore,
     MetricBleurtScore.metric_name(): MetricBleurtScore,
-    MetricQuestEval.metric_name(): MetricQuestEval,
-    MetricQuestEval_t2t.metric_name(): MetricQuestEval_t2t,
-    MetricQuestEval_t2t_src.metric_name(): MetricQuestEval_t2t_src,
-
-    MetricQuestEval_t2t_f1.metric_name(): MetricQuestEval_t2t_f1,
-    MetricQuestEval_t2t_answ.metric_name(): MetricQuestEval_t2t_answ,
-    MetricQuestEval_t2t_berscore.metric_name(): MetricQuestEval_t2t_berscore,
-    MetricQuestEval_t2t_f1_answ.metric_name(): MetricQuestEval_t2t_f1_answ,
-    MetricQuestEval_t2t_src_f1_answ.metric_name(): MetricQuestEval_t2t_src_f1_answ,
-
-    MetricQuestEval_src_weighter.metric_name(): MetricQuestEval_src_weighter,
+    #MetricQuestEval.metric_name(): MetricQuestEval,
 }
